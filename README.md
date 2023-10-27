8bae8f1126c9bb2198dce73566ea453ca5f6aff9
keyanap <keyanapregent@gmail.com>
2023-10-26 23:37:54 -0400

    Final API and read.me

--- README.md
+++ README.md
@@ -11,6 +11,30 @@ Station Analysis
 
 Part 2: Design Your Climate App
 
+Home Page
+
+![Alt text](<Screenshot 2023-10-26 at 11.25.42 PM.png>)
+
+Precipitation Route
+
+![Alt text](<Screenshot 2023-10-26 at 11.25.54 PM.png>)
+
+Stations Route
+
+![Alt text](<Screenshot 2023-10-26 at 11.26.03 PM.png>)
+
+Tobs Route
+
+![Alt text](<Screenshot 2023-10-26 at 11.26.16 PM.png>)
+
+Start Route
+
+![Alt text](<Screenshot 2023-10-26 at 11.27.19 PM.png>)
+
+Start/End Route
+
+![Alt text](<Screenshot 2023-10-26 at 11.27.45 PM.png>)
+
 
 Resources Used 
 
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -19,7 +19,7 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
-Measurement = Base.classes.measurment
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 #################################################
@@ -33,21 +33,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    f"Available Routes for Hawaii Weather:<br/>"
-    f"/api/v1.0/precipitation<br/>"
-    f"/api/v1.0/stations<br/>"
-    f"/api/v1.0/tobs<br/>"
-    f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
-    f"/api/v1.0/<start_date format:yyyy-mm-dd]/<end_date format:yyyy-mm-dd]<br/>"
-
+# List routes
+    return(
+        f"Available Routes for Hawaii Weather:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date><br/>"
+        f"/api/v1.0/<start_date>/<end_date><br/>"
+    )
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 # Create our session (link) from Python to the DB
     session = Session(engine)
 
 # Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
-    precipititaion_query = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= '2016-08-23').all()
+    precipititaion_query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').\
+        group_by(Measurement.date).order_by(Measurement.date).all()
     session.close()
 
 # Return the JSON representation of your dictionary.
@@ -58,11 +60,12 @@ def precipitation():
         precipititaion_dict['precipitation'] = prcp
         precipititaion_dict['date'] = date
         precipititaion_results.append(precipititaion_dict)
+
     return jsonify(precipititaion_results)
 
 
 @app.route("/api/v1.0/stations")
-def stations():
+def station():
 # Create our session (link) from Python to the DB
     session = Session(engine)
     station_query = session.query(Station.station, Station.id).all()
@@ -75,36 +78,73 @@ def stations():
         stations_dict['station'] = station
         stations_dict['id'] = id
         stations_results.append(stations_dict)
+
     return jsonify (stations_results)
 
 
 @app.route("/api/v1.0/tobs")
 def tobs():
 # Create our session (link) from Python to the DB
-session = Session(engine)
-
+    session = Session(engine)
 # Query the dates and temperature observations of the most-active station for the previous year of data.
+    most_active_query = session.query(Measurement.date, Measurement.tobs, Measurement.prcp).\
+        filter(Measurement.date >= '2016-08-23').\
+        filter(Measurement.station == 'USC00519281').\
+        order_by(Measurement.date).all()
+    session.close()
 
 # Return a JSON list of temperature observations for the previous year.
+    tobs_results = []
+    for date, tobs, prcp in most_active_query:
+        tobs_dict = {}
+        tobs_dict['date'] = date
+        tobs_dict['tobs'] = tobs
+        tobs_dict['prcp'] = prcp
+        tobs_results.append(tobs_dict)
 
-session.close()
+    return jsonify(tobs_results)
 
-@app.route("/api/v1.0/[start_date format:yyyy-mm-dd]")
-def start():
+
+@app.route("/api/v1.0/<start_date>")
+def start_date(start_date, end_date='2017-08-23'):
 # Create our session (link) from Python to the DB
-session = Session(engine)
+    session = Session(engine)
+    start_date_query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    session.close()
 
 # Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
-
 # For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+    start_date_results = []
+    for tmin, tavg, tmax in start_date_query:
+        start_date_dict = {}
+        start_date_dict["min_temp"] = tmin
+        start_date_dict["avg_temp"] = tavg
+        start_date_dict["max_temp"] = tmax
+        start_date_results.append(start_date_dict)
 
-session.close()
-
-@app.route("/api/v1.0/<start_date format:yyyy-mm-dd]/<end_date format:yyyy-mm-dd]")
-def startend():
+    return jsonify(start_date_results)
+    
+    
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def startend(start_date, end_date):
 # Create our session (link) from Python to the DB
-session = Session(engine)
+    session = Session(engine)
+    start_end_query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).\
+        filter(Measurement.date <= end_date).all()   
+    session.close()
 
 # For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+    start_end_results = []
+    for tmin, tavg, tmax in start_end_query:
+        start_end_dict = {}
+        start_end_dict["min_temp"] = tmin
+        start_end_dict["avg_temp"] = tavg
+        start_end_dict["max_temp"] = tmax
+        start_end_results.append(start_end_dict)
 
-session.close()
+    return jsonify(start_end_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
